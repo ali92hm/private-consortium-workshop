41e509ac240de1a195aed0cae7885aebd2a86928
ali92hm <ali92hm@gmail.com>
2017-09-06 13:48:05 -0700

    added three nodes

--- blockchain/mine.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-geth --datadir ./data --rpc --mine --rpcaddr 0.0.0.0 --nodiscover --networkid 8222017 console
--- blockchain/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-geth --datadir ./data --rpc --ipcpath ${HOME}/Library/Ethereum/geth.ipc --nodiscover --networkid 8222017 console
